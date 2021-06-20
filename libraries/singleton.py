cd9e0993bc23e8112a1ad48fbe00b3373261517b
ratul <ratul.debnath.year@gmail.com>
2021-06-21 01:30:14 +0600

    singleton.py method_memory

--- libraries/singleton.py
+++ libraries/singleton.py
@@ -1,13 +1,96 @@
 import atexit
 import hashlib
+import multiprocessing.shared_memory
 import os
 import sys
 import tempfile
 import time
 import typing
 
+
+def _dummy_callback():
+    pass
+
+
+def _wait_or_exit(end_time: float,
+                  wait_callback: typing.Callable,
+                  wait_callback_args: tuple,
+                  wait_callback_kwargs: dict[str, typing.Any],
+                  exit_callback: typing.Callable,
+                  exit_callback_args: tuple,
+                  exit_callback_kwargs: dict[str, typing.Any]) -> typing.Optional[typing.NoReturn]:
+    if end_time > time.time():
+        wait_callback(*wait_callback_args, **wait_callback_kwargs)
+        time.sleep(DELAY)
+    else:
+        exit_callback(*exit_callback_args, **exit_callback_kwargs)
+        raise SystemExit
+
+
+def method_file(name_prefix: str,
+                wait: bool,
+                crash_callback: typing.Callable,
+                crash_callback_args: tuple,
+                crash_callback_kwargs: dict[str, typing.Any],
+                wait_callback: typing.Callable,
+                wait_callback_args: tuple,
+                wait_callback_kwargs: dict[str, typing.Any],
+                exit_callback: typing.Callable,
+                exit_callback_args: tuple,
+                exit_callback_kwargs: dict[str, typing.Any]) -> typing.Optional[typing.NoReturn]:
+    temp_dir = tempfile.gettempdir()
+    os.makedirs(temp_dir, exist_ok=True)
+    path = os.path.join(temp_dir, f'{name_prefix}_{get_uid()}.lock')
+    end_time = time.time() + (wait or False) * MAX_WAIT - DELAY
+    while True:
+        try:
+            file = os.open(path, os.O_CREAT | os.O_EXCL)
+        except FileExistsError:
+            try:
+                os.remove(path)
+            except PermissionError:
+                _wait_or_exit(end_time,
+                              wait_callback, wait_callback_args, wait_callback_kwargs,
+                              exit_callback, exit_callback_args, exit_callback_kwargs)
+                continue
+            else:
+                crash_callback(*crash_callback_args, **crash_callback_kwargs)
+                continue
+        atexit.register(os.remove, path)
+        atexit.register(os.close, file)
+        break
+
+
+def method_memory(name_prefix: str,
+                  wait: bool,
+                  _: typing.Callable,
+                  __: tuple,
+                  ___: dict[str, typing.Any],
+                  wait_callback: typing.Callable,
+                  wait_callback_args: tuple,
+                  wait_callback_kwargs: dict[str, typing.Any],
+                  exit_callback: typing.Callable,
+                  exit_callback_args: tuple,
+                  exit_callback_kwargs: dict[str, typing.Any]) -> typing.Optional[typing.NoReturn]:
+    name = f'{name_prefix}_{get_uid()}.lock'
+    end_time = time.time() + (wait or False) * MAX_WAIT - DELAY
+    while True:
+        try:
+            memory = multiprocessing.shared_memory.SharedMemory(name, True, 1)
+        except FileExistsError:
+            _wait_or_exit(end_time,
+                          wait_callback, wait_callback_args, wait_callback_kwargs,
+                          exit_callback, exit_callback_args, exit_callback_kwargs)
+            continue
+        atexit.register(memory.unlink)
+        atexit.register(memory.close)
+        break
+
+
+# TODO: add port, mutex method
 DELAY = 1
 MAX_WAIT = 5
+METHOD = method_file
 
 
 def get_uid(path: str = os.path.normpath(sys.argv[0])) -> str:
@@ -26,33 +109,7 @@ def init(name_prefix: str = os.path.basename(sys.argv[0]),
          exit_callback: typing.Optional[typing.Callable] = None,
          exit_callback_args: typing.Optional[tuple] = None,
          exit_callback_kwargs: typing.Optional[dict[str, typing.Any]] = None) -> typing.Optional[typing.NoReturn]:
-    temp_dir = tempfile.gettempdir()
-    os.makedirs(temp_dir, exist_ok=True)
-    file_path = os.path.join(temp_dir, f'{name_prefix}_{get_uid()}.lock')
-    retry = True
-    end_time = time.time() + (wait or False) * MAX_WAIT - DELAY
-    while retry:
-        try:
-            file = os.open(file_path, os.O_CREAT | os.O_EXCL)
-        except FileExistsError:
-            try:
-                os.remove(file_path)
-            except PermissionError:
-                retry = end_time > time.time()
-                if retry:
-                    if wait_callback:
-                        wait_callback(*wait_callback_args or (), **wait_callback_kwargs or {})
-                    time.sleep(DELAY)
-                    continue
-                if exit_callback:
-                    exit_callback(*exit_callback_args or (), **exit_callback_kwargs or {})
-                raise SystemExit
-            else:
-                if crash_callback:
-                    crash_callback(*crash_callback_args or (), **crash_callback_kwargs or {})
-                continue
-        retry = False
-        atexit.register(os.remove, file_path)
-        atexit.register(os.close, file)
-
-# TODO: add port, mutex method
+    METHOD(name_prefix, bool(wait),
+           crash_callback or _dummy_callback, crash_callback_args or (), crash_callback_kwargs or {},
+           wait_callback or _dummy_callback, wait_callback_args or (), wait_callback_kwargs or {},
+           exit_callback or _dummy_callback, exit_callback_args or (), exit_callback_kwargs or {})
